9b8f3ce146404b58d0f1ec28a77a1f051ddff13e
oscarramosp@gmail.com <oscarramosp@gmail.com@7bdf12f4-a304-94ce-ff84-7cbc3e31cfba>
2013-12-02 16:11:22 +0000

    Fx

--- SIDWeb/sid/Master.Master.designer.cs
+++ SIDWeb/sid/Master.Master.designer.cs
@@ -14,6 +14,15 @@ namespace sid {
     public partial class Master {
         
         /// <summary>
+        /// Head1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        
+        /// <summary>
         /// head control.
         /// </summary>
         /// <remarks>
