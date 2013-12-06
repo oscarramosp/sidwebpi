faa3e49b2a98457be2bc1a3db9bd7292658d2712
oscarramosp@gmail.com <oscarramosp@gmail.com@7bdf12f4-a304-94ce-ff84-7cbc3e31cfba>
2013-12-06 07:21:12 +0000

    Reporte

---
+++ SIDWeb/BLLayer/BLCanilla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Common;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using DALayer;
+using BELayer;
+using System.Data;
+
+
+namespace BLLayer
+{
+    public class BLCanilla
+    {
+        public DataSet listarCanillas()
+        {
+            DACanilla oDACanilla = new DACanilla();
+            try
+            {
+                return oDACanilla.listarCanillas();
+            }
+            catch (Exception ex)
+            {
+                ExceptionPolicy.HandleException(ex, "Policy");
+                return null;
+            }
+        }
+    }
+}
