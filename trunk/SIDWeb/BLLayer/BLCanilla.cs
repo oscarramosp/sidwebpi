a205697b11dd52523f6999b8d1c352759688153c
oscarramosp@gmail.com <oscarramosp@gmail.com@7bdf12f4-a304-94ce-ff84-7cbc3e31cfba>
2014-03-08 21:20:27 +0000

    avance búsuqeda canillas

--- trunk/SIDWeb/BLLayer/BLCanilla.cs
+++ trunk/SIDWeb/BLLayer/BLCanilla.cs
@@ -13,12 +13,26 @@ namespace BLLayer
 {
     public class BLCanilla
     {
-        public DataSet listarCanillas()
+        public DataSet reporteCanillas()
         {
             DACanilla oDACanilla = new DACanilla();
             try
             {
-                return oDACanilla.listarCanillas();
+                return oDACanilla.reporteCanillas();
+            }
+            catch (Exception ex)
+            {
+                ExceptionPolicy.HandleException(ex, "Policy");
+                return null;
+            }
+        }
+
+        public List<BECanilla> selectCanillas(BECanilla canilla)
+        {
+            DACanilla oDACanilla = new DACanilla();
+            try
+            {
+                return oDACanilla.selectCanillas(canilla);
             }
             catch (Exception ex)
             {
